0189c9e0e07268fd55104a064b5058ca7d719cd2
Nimaoth <nicolas.otremba@web.de>
2026-02-25 11:14:20 +0100

    Add setting to return all workspace symbols
    This adds a setting to tell the language server to
    return all symbols for the "workspace/symbol" request,
    and not perform any filtering in the language server.
    VSCode will filter the results instead.
    
    The filtering in the language server is quite primitive
    and doesn't allow many typos, so letting vscode
    filter the results is generally better.
    
    It's still an option to let the language server
    filter results because if the number of symbols is quite large
    then that might be faster, although in practices just filtering
    in VSCode seems fast enough for now

--- language-server/src/server.ts
+++ language-server/src/server.ts
@@ -1192,6 +1192,11 @@ connection.onDidChangeConfiguration(function (change : DidChangeConfigurationPar
     if (dirtyDiagnostics)
         DirtyAllDiagnostics();
 
+// Aesir Mod: Add setting to return all workspace symbols
+    let symbolSettings = scriptsymbols.GetSymbolSettings();
+    symbolSettings.returnAllWorkspaceSymbols = settings.returnAllWorkspaceSymbols;
+// END Aesir Mod: Add setting to return all workspace symbols
+
     if (port != settings.unrealConnectionPort)
     {
         port = settings.unrealConnectionPort;
